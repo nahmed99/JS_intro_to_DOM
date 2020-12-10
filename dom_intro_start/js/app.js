b225e556cd70a61d2fd7238ea32aca7d5660d146
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-10 12:02:22 +0000

    Some dom work done in JavaScript

--- dom_intro_start/js/app.js
+++ dom_intro_start/js/app.js
@@ -1,1 +1,31 @@
+// Wait for the DOM to be loaded before running the JS code.
 
+// Manipulate the h1 tag - this will only change the first one
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Using querySelector to 'grab' elements/objects
+    // querySelector('#element-by-id')
+    // querySelector('.element-by-class')
+    // querySelector('html tag')
+
+    /*
+    const theH1 = document.querySelector('h1');
+    theH1.textContent = "JavaScript say Kidda?";
+
+
+    const welcomeParagraph = document.querySelector('#welcome-paragraph');
+    console.log(welcomeParagraph);
+    console.dir(welcomeParagraph);
+    */
+
+    // Grabbing the 'li' tag of the '.red' class
+    const redLitsItem = document.querySelector('li.red');
+
+    redLitsItem.textContent = "RED!";
+
+    
+
+
+})
+
+console.log("Kidda?");
